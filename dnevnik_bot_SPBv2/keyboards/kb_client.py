1fc16ee103182cf2ebd774b08b54734b2fa05a8b
kotafan1rich <lesapond2007@gmail.com>
2024-12-22 00:40:58 +0300

    Changes to be committed: 	modified:   dnevnik_bot_SPBv2/Dockerfile 	modified:   dnevnik_bot_SPBv2/keyboards/kb_client.py 	modified:   dnevnik_bot_SPBv2/requirements.txt 	modified:   docker-compose.yaml 	new file:   redis/redis.conf 	new file:   redis/users.acl

--- dnevnik_bot_SPBv2/keyboards/kb_client.py
+++ dnevnik_bot_SPBv2/keyboards/kb_client.py
@@ -1,5 +1,7 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
+from aiocache import cached
+
 from config import API_URL
 
 help_b = KeyboardButton(text="Помощь")
@@ -22,7 +24,7 @@ async def get_kb_clent_periods_bottoms(id_tg, session):
 		data: dict = await response.json()
 		return [KeyboardButton(text=period) for period in data.get("result").keys()]
 
-
+@cached(ttl=600)
 async def get_kb_client_main(id_tg, session):
 	periods = await get_kb_clent_periods_bottoms(id_tg, session)
 	kb_bottoms = [periods, [settings_b, help_b]]
