d461be380fbb81637cbeacf9472d24bc209f6807
kotafan1rich <lesapond2007@gmail.com>
2025-01-31 19:17:08 +0300

    logger

--- dnevnik_bot_SPBv2/create_bot.py
+++ dnevnik_bot_SPBv2/create_bot.py
@@ -1,9 +1,15 @@
+import logging
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from config import TOKEN
 
 storage = MemoryStorage()
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
 
 bot = Bot(token=TOKEN, parse_mode="HTML")
 dp = Dispatcher(storage=storage)
